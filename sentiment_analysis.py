c6f505a9c8092df455aae2fb57c541735454ad97
Jan-Paul Peters <j.p.j.peters@student.tudelft.nl>
2025-01-09 16:14:37 +0100

    added threshold to discard entries with too few sentiments

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -5,19 +5,21 @@ def load_data(file_path):
         data = json.load(file)
     return data
 
-def find_most_liked_disliked(data):
+def find_most_liked_disliked(data, threshold = 10):
     most_liked = []
     most_disliked = []
     max_relative_positive = float('-inf')
     max_relative_negative = float('-inf')
 
     for stock, sentiment in data.items():
-        total_sentiment = sentiment['positive'] +  sentiment['negative'] + sentiment['neutral']
+        total_sentiments = sentiment['positive'] +  sentiment['negative'] + sentiment['neutral']
+        if total_sentiments < threshold:
+            continue
 
-        relative_positive = sentiment['positive']/total_sentiment
-        relative_negative = sentiment['negative']/total_sentiment
+        relative_positive = sentiment['positive']/total_sentiments
+        relative_negative = sentiment['negative']/total_sentiments
 
-        print(stock, relative_positive, relative_negative)
+        print(stock, total_sentiments, relative_positive, relative_negative)
         
         if relative_positive > max_relative_positive:
             max_relative_positive = relative_positive
@@ -34,9 +36,12 @@ def find_most_liked_disliked(data):
     return most_liked, most_disliked, max_relative_positive, max_relative_negative
 
 if __name__ == "__main__":
+    threshold = 10
+
     file_path = 'sentiment/stocks_sentiment.json'  # Replace with your JSON file path
     data = load_data(file_path)
-    most_liked, most_disliked, max_positive, max_negative = find_most_liked_disliked(data)
-    print(f"Most liked stock: {most_liked}, {max_positive}")
+    most_liked, most_disliked, max_relative_positive, max_relative_negative = find_most_liked_disliked(data, threshold= threshold)
     print('-----------------------------------')
-    print(f"Most disliked stock: {most_disliked}, {max_negative}")
+    print(f"Most liked: {most_liked}, {max_relative_positive}")
+    print('-----------------------------------')
+    print(f"Most disliked: {most_disliked}, {max_relative_negative}")
